58894635318158b93fc21585821b82b02cc365b2
wangyannhao <wangyannhao@gmail.com>
2018-03-27 18:42:41 -0400

    update getReserv

--- application.py
+++ application.py
@@ -240,54 +240,64 @@ def get_reserv():
     conn = mysql.connect()
     cursor = conn.cursor()
     res = {}
-    try:
-        account_no = request.form['account_no']
-        cursor.execute('SELECT DISTINCT name,ssn,Reservation.reservation_no, FlightInfoAll.departure, FlightInfoAll.arrival FROM Reservation_Leg JOIN Reservation JOIN LegsInfo JOIN FlightInfoAll ON Reservation.reservation_no=Reservation_Leg.reservation_no AND Reservation_Leg.idLegs=LegsInfo.idLegs AND LegsInfo.idFlight=FlightInfoAll.idFlightInfo WHERE Reservation.account_no= %s;',[account_no])
-        res_no = []
-        for data in cursor.fetchall():
-            if(data):
-                if(data[2] not in res.keys()):
-                    res_no.append(data[2])
-                    res[data[2]] = {}
-                    res[data[2]]['passenger_info'] = []
-                    res[data[2]]['stops'] = {}
-                    res[data[2]]['stops']['go'] = []
-                    res[data[2]]['stops']['back'] = []
-                    res[data[2]]['RoundTrip'] = False
-                res[data[2]]['Departure'] = data[3]
-                res[data[2]]['Arrival'] = data[4]
-                temp = {}
-                temp['name'] = data[0]
-                temp['ssn'] = data[1]
-                res[data[2]]["passenger_info"].append(temp)
-        cursor.execute('SELECT distinct departure_airport,arrival_airport,departure_time,arrival_time,flight_no, airlineCode, airlineName, booking_fee, total_fare, trip_no, duration ,distance, date, reservation_no   from Reservation natural join Reservation_Leg natural join LegsInfo where account_no = %s;',[account_no])
-        for data in cursor.fetchall():
-                dict = {}
-                dict['departure_airport'] = data[0]
-                dict['arrival_airport'] = data[1]
-                dict['departure_time'] = data[2]
-                dict['arrival_time'] = data[3]
-                dict['flight_no'] = data[4]
-                dict['airlineCode'] = data[5]
-                dict['airlineName'] = data[6]
-                dict['booking_fee'] = data[7]
-                dict['total_fare'] = data[8]
-                res[data[13]]['price']= data[8]
-                # dict['trip_no'] = data[9]
-                dict['duration'] = data[10]
-                dict['distance'] = data[11]
-                dict['date'] = data[12]
-                if(data[9]==1):
-                    res[data[13]]['stops']['go'].append(dict)
-                else:
-                    res[data[13]]['stops']['back'].append(dict)
-                    res[data[13]]['RoundTrip'] = True
-    except Exception as e:
-        res['error'] = 'Search Error'
-    finally:
-        cursor.close()
-        conn.close()
-        return jsonify(res)
+# try:
+    account_no = request.form['account_no']
+    cursor.execute('SELECT DISTINCT name,ssn,Reservation.reservation_no, FlightInfoAll.departure, FlightInfoAll.arrival FROM Reservation_Leg JOIN Reservation JOIN LegsInfo JOIN FlightInfoAll ON Reservation.reservation_no=Reservation_Leg.reservation_no AND Reservation_Leg.idLegs=LegsInfo.idLegs AND LegsInfo.idFlight=FlightInfoAll.idFlightInfo WHERE Reservation.account_no= %s;',[account_no])
+    # res_no = []
+    res_dict = {}
+    res_list = []
+    i = 0;
+    for data in cursor.fetchall():
+        if(data):
+            if(data[2] not in res_dict.keys()):
+                res_list.append({})
+                res_dict[data[2]] = i
+                res_list[res_dict[data[2]]]['id'] = data[2]
+                res_list[res_dict[data[2]]]['passenger_info'] = []
+                res_list[res_dict[data[2]]]['stops'] = {}
+                res_list[res_dict[data[2]]]['stops']['go'] = []
+                res_list[res_dict[data[2]]]['stops']['back'] = []
+                res_list[res_dict[data[2]]]['RoundTrip'] = False
+                i+=1
+
+            res_list[res_dict[data[2]]]['Departure'] = data[3]
+            res_list[res_dict[data[2]]]['Arrival'] = data[4]
+            temp = {}
+            temp['name'] = data[0]
+            temp['ssn'] = data[1]
+            res_list[res_dict[data[2]]]["passenger_info"].append(temp)
+
+
+    cursor.execute('SELECT distinct departure_airport,arrival_airport,departure_time,arrival_time,flight_no, airlineCode, airlineName, booking_fee, total_fare, trip_no, duration ,distance, date, reservation_no   from Reservation natural join Reservation_Leg natural join LegsInfo where account_no = %s;',[account_no])
+    for data in cursor.fetchall():
+            dict = {}
+            dict['departure_airport'] = data[0]
+            dict['arrival_airport'] = data[1]
+            dict['departure_time'] = data[2]
+            dict['arrival_time'] = data[3]
+            dict['flight_no'] = data[4]
+            dict['airlineCode'] = data[5]
+            dict['airlineName'] = data[6]
+            dict['booking_fee'] = data[7]
+            dict['total_fare'] = data[8]
+            # dict['trip_no'] = data[9]
+            dict['duration'] = data[10]
+            dict['distance'] = data[11]
+            dict['date'] = data[12]
+            j = res_dict[data[13]]
+            res_list[j]['price']= data[8]
+            if(data[9]==1):
+                res_list[j]['stops']['go'].append(dict)
+            else:
+                res_list[j]['stops']['back'].append(dict)
+                res_list[j]['RoundTrip'] = True
+# except Exception as e:
+#     res['error'] = 'Search Error'
+# finally:
+    cursor.close()
+    conn.close()
+    print res_dict
+    return jsonify(res_list)
 
 # Finished
 # Get Travel Initary
