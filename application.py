5e871ab8079a569547cdb9cf2adac21cf335203c
sjhdolores <sjhdolores@163.com>
2018-03-27 20:36:06 -0400

    manager/mostActiveFlight

--- application.py
+++ application.py
@@ -286,9 +286,28 @@ def get_most_rev():
         conn.close()
     return jsonify(res)
 
+#finished
 @application.route('/api/manager/mostActiveFlight',methods=['POST','GET'])
 def most_active_flight():
-    return ""
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    res = []
+    try:
+        cursor.execute("SELECT flight_no, COUNT(*) as num, airlineCode FROM LegsInfo, Reservation_Leg WHERE LegsInfo.idLegs=Reservation_Leg.idLegs GROUP BY flight_no ORDER BY num DESC")
+        for data in cursor.fetchmany(1):
+            print data
+            dic = {}
+            dic["flight_no"] = data[0]
+            dic["active_number"] = data[1]
+            dic["airlineCode"] = data[2]
+        res.append(dic)
+    except Exception as e:
+        print e
+        res = ["Search Error"]
+    finally:
+        cursor.close()
+        conn.close()
+    return jsonify(res)
 
 #finished
 @application.route('/api/manager/listForAirport',methods=['POST','GET'])
