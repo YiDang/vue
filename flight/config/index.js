c1292b9cdb721c607c0afa7e6f8107dd42ac2a71
YiDang <dangyi0811@hotmail.com>
2018-04-01 17:28:57 -0400

    todo:deploy

--- flight/config/index.js
+++ flight/config/index.js
@@ -11,20 +11,20 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-        '/api': {
-            // target: 'http://172.31.193.215:5000/',//设置你调用的接口域名和端口号 别忘了加http
-            target: 'http://student-universe.us-east-1.elasticbeanstalk.com/',//设置你调用的接口域名和端口号 别忘了加http
-            // target: 'http://172.31.194.146:5000/',//设置你调用的接口域名和端口号 别忘了加http
-        // target: 'http://172.31.198.208:5000/',//设置你调用的接口域名和端口号 别忘了加http
-        // target: 'http://172.31.217.129:8000/',//设置你调用的接口域名和端口号 别忘了加http
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/'
-                //这里理解成用‘/api’代替target里面的地址，
-                // 后面组件中我们掉接口时直接用api代替 比如我要调
-                // 用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
-        }
-      }
+      //   '/api': {
+      //       // target: 'http://172.31.193.215:5000/',//设置你调用的接口域名和端口号 别忘了加http
+      //       // target: 'http://student-universe.us-east-1.elasticbeanstalk.com/',//设置你调用的接口域名和端口号 别忘了加http
+      //       target: 'http://172.31.197.159:5000/',//设置你调用的接口域名和端口号 别忘了加http
+      //   // target: 'http://172.31.198.208:5000/',//设置你调用的接口域名和端口号 别忘了加http
+      //   // target: 'http://172.31.217.129:8000/',//设置你调用的接口域名和端口号 别忘了加http
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     '^/api': '/'
+      //           //这里理解成用‘/api’代替target里面的地址，
+      //           // 后面组件中我们掉接口时直接用api代替 比如我要调
+      //           // 用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+      //   }
+      // }
     },
 
     // Various Dev Server settings
--- flight/src/components/prefix.js
+++ flight/src/components/prefix.js
@@ -1,3 +1,4 @@
 // export const prefix='/api'
-var prefix = '' 
+var prefix = ''
+// var prefix = '/api' 
 export {prefix}
